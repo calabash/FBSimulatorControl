644ffacc213a3d7dc4a0942ae4d90ba51e152fc9
Joshua Moody <joshuajmoody@gmail.com>
2016-11-18 12:08:57 +0100

    CalabashUtils: remove unused log file method

--- Calabash/CalabashUtils.m
+++ Calabash/CalabashUtils.m
@@ -23,29 +23,4 @@
     }
 }
 
-//Try to store logs at "${HOME}/.calabash/iOSDeviceManager/logs"
-+ (NSString *)logfileLocation:(NSError *__autoreleasing *)err {
-    NSString *errStr = nil;
-    NSString *logsDir = [[[NSHomeDirectory()
-                           stringByAppendingPathComponent:@".calabash"]
-                          stringByAppendingPathComponent:@"iOSDeviceManager"]
-                         stringByAppendingPathComponent:@"logs"];
-    
-    NSFileManager *fm = [NSFileManager defaultManager];
-    if (![fm fileExistsAtPath:logsDir]) {
-        NSError *e;
-        [fm createDirectoryAtPath:logsDir withIntermediateDirectories:YES attributes:nil error:&e];
-        if (e) {
-            errStr = [NSString stringWithFormat:@"Error creating logging dir at %@: %@", logsDir, e];
-        }
-    }
-    if (errStr) {
-        logsDir = nil;
-        *err = [NSError errorWithDomain:@"sh.calaba.iOSDeviceManager.LoggingError"
-                                   code:27753
-                               userInfo:@{NSLocalizedDescriptionKey: errStr}];
-    }
-    return logsDir;
-}
-
 @end
